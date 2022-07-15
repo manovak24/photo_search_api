11736ad9ee85babe57e0b9d517b4ca7d2b089d05
emagdnimMQT <mark.w.novak@gmail.com>
2022-07-15 13:46:19 -0400

    Commented out the xml request for now until rest is finished.

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const pexelKey = config.PEXEL_API;
 
 const url = "https://api.pexels.com/v1/search?query=people";
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-
-       const data = JSON.parse(xhttp.responseText);
-       console.log(data);
-       document.getElementById("demo").innerHTML = `
-            <img src="${data.photos[0].src.small}">
-       `;
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//        // Typical action to be performed when the document is ready:
+//        const data = JSON.parse(xhttp.responseText);
+//        console.log(data);
+//        document.getElementById("demo").innerHTML = `
+//             <img src="${data.photos[0].src.small}">
+//        `;
        
-    }
-};
-xhttp.open("GET", url, true);
-xhttp.setRequestHeader('Authorization', pexelKey)
-xhttp.send();
+//     }
+// };
+// xhttp.open("GET", url, true);
+// xhttp.setRequestHeader('Authorization', pexelKey)
+// xhttp.send();
