ebec28b16a0e2e89cb8599040911c4d8b3d3e3bc
emagdnimMQT <mark.w.novak@gmail.com>
2022-08-05 15:19:51 -0400

    getting closer.

--- main.js
+++ main.js
@@ -5,67 +5,82 @@ const columnTwo = document.querySelector('.photos-column-two');
 const columnThree = document.querySelector('.photos-column-three');
 const searchCtr = document.querySelector('.search-bar-ctr');
 const submitBtn = document.querySelector('.submit-btn');
-const trendingItems = document.querySelectorAll('.trending-item');
+const trendingItems = document.getElementById('trending-item-one');
 
-const onPageLoad = (e) => {
-    e.preventDefault();
-    let term = 'Beautiful';
-    const url = `https://api.pexels.com/v1/search?query=${term}&per_page=24`;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const data = JSON.parse(xhttp.responseText);
-            console.log(data);
-            console.log(data.photos);
+let term;
 
-            for(let i = 0; i < data.photos.length; i++) {
-                const photoDiv = document.createElement('div');
-                photoDiv.classList.add('img-ctr');
-                if(i <= 7) {
-                    photoDiv.innerHTML = `
-                        <img
-                            class="pexel-img"
-                            lazy="load"
-                            src=${data.photos[i].src.large}
-                            alt=${data.photos[i].alt}
-                        >
-                    `;
-                    columnOne.appendChild(photoDiv);
-                } else if(i > 7 && i <= 15) {
-                    photoDiv.innerHTML = `
-                        <img
-                            class="pexel-img" 
-                            lazy="load"
-                            src=${data.photos[i].src.large}
-                            alt=${data.photos[i].alt}
-                        >
-                    `;
-                    columnTwo.appendChild(photoDiv);
-                } else {
-                    photoDiv.innerHTML = `
-                        <img
-                            class="pexel-img" 
-                            lazy="load"
-                            src=${data.photos[i].src.large}
-                            alt=${data.photos[i].alt}
-                        >
-                    `;
-                    columnThree.appendChild(photoDiv);
-                }
-            }
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.setRequestHeader('Authorization', pexelKey)
-    xhttp.send();
-}
+// The below api is for when page loads and to bring up images
+
+// const onPageLoad = (e) => {
+//     e.preventDefault();
+//     let term = 'Beautiful';
+//     const url = `https://api.pexels.com/v1/search?query=${term}&per_page=24`;
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             const data = JSON.parse(xhttp.responseText);
+//             console.log(data);
+//             console.log(data.photos);
+
+//             for(let i = 0; i < data.photos.length; i++) {
+//                 const photoDiv = document.createElement('div');
+//                 photoDiv.classList.add('img-ctr');
+//                 if(i <= 7) {
+//                     photoDiv.innerHTML = `
+//                         <img
+//                             class="pexel-img"
+//                             lazy="load"
+//                             src=${data.photos[i].src.large}
+//                             alt=${data.photos[i].alt}
+//                         >
+//                     `;
+//                     columnOne.appendChild(photoDiv);
+//                 } else if(i > 7 && i <= 15) {
+//                     photoDiv.innerHTML = `
+//                         <img
+//                             class="pexel-img" 
+//                             lazy="load"
+//                             src=${data.photos[i].src.large}
+//                             alt=${data.photos[i].alt}
+//                         >
+//                     `;
+//                     columnTwo.appendChild(photoDiv);
+//                 } else {
+//                     photoDiv.innerHTML = `
+//                         <img
+//                             class="pexel-img" 
+//                             lazy="load"
+//                             src=${data.photos[i].src.large}
+//                             alt=${data.photos[i].alt}
+//                         >
+//                     `;
+//                     columnThree.appendChild(photoDiv);
+//                 }
+//             }
+//         }
+//     };
+//     xhttp.open("GET", url, true);
+//     xhttp.setRequestHeader('Authorization', pexelKey)
+//     xhttp.send();
+// }
+
+// window.onload = function(e) {
+//     onPageLoad(e);
+// }
+
+// The below api request is for using the search bar
 
 const searchEvent = (e) => {
     e.preventDefault();
     columnOne.innerHTML = ``;
     columnTwo.innerHTML = ``;
     columnThree.innerHTML = ``;
-    let term = searchTerm.value;
+
+    if(!searchTerm.value) {
+        term = trendingItems.innerText;
+    } else {
+        term = searchTerm.value;
+    }
     const url = `https://api.pexels.com/v1/search?query=${term}&per_page=24`;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -131,18 +146,27 @@ const searchEvent = (e) => {
     xhttp.send();
 }
 
-window.onload = function(e) {
-    onPageLoad(e);
-}
-
 searchCtr.addEventListener('submit', (e) => {
     searchEvent(e);
+    searchTerm.value = '';
 });
 
+
+
+// Testing out anime.js and added to hero-content
 anime({
-    targets: '.hero-title',
+    targets: '.hero-content',
     translateY: [
-        { value: -50, duration: 1000 },
+        { value: -20, duration: 1000 },
         { value: 0, duration: 800}
-    ]
+    ],
+    direction: 'alternate',
+    loop: 'false',
+    easing: 'linear'
 });
+
+trendingItems.addEventListener('click', function(e) {
+    console.log(e.currentTarget)
+    searchEvent(e)
+})
+
