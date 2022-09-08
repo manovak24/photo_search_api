b7788d02798e3a0225221992c9da52dbc700d1c0
emagdnimMQT <mark.w.novak@gmail.com>
2022-09-08 13:05:28 -0400

    Added event handlers for search bar containers but main search bar container isn't working.

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 const pexelKey = config.PEXEL_API;
 const searchTerm = document.querySelector('#search-term');
 const overlaySearchTerm = document.querySelector('#overlay-search-term');
+const mobileSearchTerm = document.querySelector('#mobile-search-term');
+const topnavSearchTerm = document.querySelector('#topnav-search-term');
 const columnOne = document.querySelector('.photos-column-one');
 const columnTwo = document.querySelector('.photos-column-two');
 const columnThree = document.querySelector('.photos-column-three');
-const searchCtr = document.querySelectorAll('.search-bar-ctr');
+const searchCtr = document.querySelector('.search-bar-ctr');
 const overlaySearchCtr = document.querySelector('.overlay-search-ctr');
+const mobileSearchCtr = document.querySelector('.mobile-search-ctr');
 const submitBtn = document.querySelector('.submit-btn');
 
 // variables for trending search items
@@ -15,6 +18,16 @@ const trendingTwo = document.getElementById('trending-two');
 const trendingThree = document.getElementById('trending-three');
 const trendingFour = document.getElementById('trending-four');
 const trendingItems = document.querySelectorAll('.trending-item');
+
+// sticky navbar
+const navBar = document.querySelector('#nav-bar');
+const navLinks = document.querySelectorAll('.nav-link');
+const joinBtn = document.querySelector('#join-btn');
+const menuIcon = document.querySelector('.menu-icon');
+const topNavSearchBarCtr = document.querySelector('.topnav-search-bar-ctr');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileSearchBarCtr = document.querySelector('.mobile-search-bar-ctr');
+const mobileJoinBtn = document.querySelector('#mobile-join-btn');
 
 // Search Term variable for the search API
 let termToSearch;
@@ -98,25 +111,37 @@ const searchEvent = (e) => {
 // }
 
 // Trying to figure out a way to use same search for all search bar containers.
+// searchCtr.forEach(search => {
+//     search.addEventListener('submit', (e) => {
+//         termToSearch = searchTerm.value;
+//         searchEvent(e);
+//         searchTerm.value = '';
+//     })
+// })
 
-searchCtr.forEach(search => {
-    search.addEventListener('submit', (e) => {
-        termToSearch = search.value;
-        searchEvent(e);
-        searchTerm.value = '';
-    })
-})
 
 
 // Event Listeners for search bar and each trending tag
-// searchCtr.addEventListener('submit', (e) => {
-//     // set term variable for api to the search term value
-//     termToSearch = searchTerm.value;
-//     // call search event function
-//     searchEvent(e);
-//     // clear the search bar for blank result after search api
-//     searchTerm.value = '';
-// });
+searchCtr.addEventListener('submit', (e) => {
+    // set term variable for api to the search term value
+    termToSearch = searchTerm.value;
+    // call search event function
+    searchEvent(e);
+    // clear the search bar for blank result after search api
+    searchTerm.value = '';
+});
+
+mobileSearchBarCtr.addEventListener('submit', (e) => {
+    termToSearch = mobileSearchTerm.value;
+    searchEvent(e);
+    mobileSearchTerm.value = '';
+})
+
+topNavSearchBarCtr.addEventListener('submit', (e) => {
+    termToSearch = topnavSearchTerm.value;
+    searchEvent(e);
+    topnavSearchTerm.value = '';
+})
 
 overlaySearchCtr.addEventListener('submit', (e) => {
     termToSearch = overlaySearchTerm.value;
@@ -148,16 +173,8 @@ trendingFour.addEventListener('click', (e) => {
     searchTerm.value = '';
 })
 
-// sticky navbar
-const navBar = document.querySelector('#nav-bar');
-const navLinks = document.querySelectorAll('.nav-link');
-const joinBtn = document.querySelector('#join-btn');
-const menuIcon = document.querySelector('.menu-icon');
-const topNavSearchBar = document.querySelector('.topnav-search-bar-ctr');
-const mobileNav = document.querySelector('.mobile-nav');
-const mobileSearchBar = document.querySelector('.mobile-search-bar-ctr');
-const mobileJoinBtn = document.querySelector('#mobile-join-btn');
 
+// Sticky Nav Bar
 window.onscroll = function () {
     addStickyNav(), removeStickyNav()
 };
@@ -173,9 +190,9 @@ function addStickyNav() {
         })
         menuIcon.classList.add('sticky-menu-icon');
         mobileNav.classList.add('sticky-mobile-nav');
-        mobileSearchBar.classList.add('sticky-mobile-search-bar-ctr');
+        mobileSearchBarCtr.classList.add('sticky-mobile-search-bar-ctr');
         joinBtn.classList.add('sticky-join-btn');
-        topNavSearchBar.classList.add('sticky-topnav-search-bar-ctr');
+        topNavSearchBarCtr.classList.add('sticky-topnav-search-bar-ctr');
     }
 }
 
@@ -189,9 +206,9 @@ function removeStickyNav() {
         })
         menuIcon.classList.remove('sticky-menu-icon');
         mobileNav.classList.remove('sticky-mobile-nav');
-        mobileSearchBar.classList.remove('sticky-mobile-search-bar-ctr');
+        mobileSearchBarCtr.classList.remove('sticky-mobile-search-bar-ctr');
         joinBtn.classList.remove('sticky-join-btn');
-        topNavSearchBar.classList.remove('sticky-topnav-search-bar-ctr');
+        topNavSearchBarCtr.classList.remove('sticky-topnav-search-bar-ctr');
     }
 }
 
